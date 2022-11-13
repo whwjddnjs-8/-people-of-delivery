4be9c1a3fb28333f26ce253f49c4f7ccc22ca5b7
조정원 <whwjddnjs8@gmail.com>
2022-11-13 17:53:50 +0900

    init

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		autoScrolling: true,
+		anchors:['home', 'first', 'second', 'third', 'fourth' , 'fifth' , 'sixth', 'footer'],
+		scrollHorizontally: true,
+		navigation: false,
+		
+		afterLoad: function (anchorLink, index) {
+			if (index == 1) {
+				$('.navi').fadeOut()
+				$('.app_download').hide()
+				$('header').fadeIn()
+			}  else if (index == 8) {
+				$('header').fadeOut()
+			} else {
+				$('header').fadeIn()
+				$('.navi').fadeIn()
+				$('.app_download').show()
+				$('.navi li').eq(index-2).children('a').addClass('active')
+				$('.navi li').eq(index-2).siblings().children('a').removeClass('active')
+			}
+
+			if (index == 2) {
+				$('.bg2 .content .bg_area img').css("left" ,"-58px")
+				$('.bg2 .content .bg_area img').css("top" ,"-58px")
+			/* 	$('.bg2 .content .bg_area:after').css("animation", "ride_bike 1s linear") */
+
+				$('.bg2 .content .bg_area img').animate({"top" : "0", "left" : "0"},{duration: 500})
+			}
+      },
+	});
+});
